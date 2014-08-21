5e61aded62f18117dbec3161f0c1e3a7198f92d0
MadnessFreak <supervivo@icloud.com>
2014-08-21 23:50:45 +0200

    Create Network Testing

---
+++ Code/Rewtek.Testing.Network/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Rewtek.GameLibrary;
+using Rewtek.GameLibrary.Network;
+
+namespace Rewtek.Testing.Network
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Core.Initialize();
+
+            var packetHandler = new PacketHandler();
+
+            var client = new NetworkClient();
+            client.Connect("211.130.155.62", 5330);
+            client.SubscribePacketHandler(packetHandler);
+            client.PacketReceived += new EventHandler<PacketEventArgs>(client_PacketReceived);
+
+            Console.ReadLine();
+        }
+
+        static void client_PacketReceived(object sender, PacketEventArgs e)
+        {
+            var packet = e.Packet;
+
+            Console.WriteLine(packet.GetString());
+        }
+    }
+}
